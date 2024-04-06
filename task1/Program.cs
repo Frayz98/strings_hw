2cf88cc05005de58e4c7f43bf91615e0eb48b244
Vladislav Vaga <frayz0498@gmail.com>
2024-04-06 09:04:39 +0500

    Решил первую задачу

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] charsArray = { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+
+string CharsArrayToString(char[,] charsArray)
+{
+    string res = "";
+
+    for (int i = 0; i < charsArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < charsArray.GetLength(1); j++)
+        {
+            res += charsArray[i, j];
+        }
+    }
+
+    return res;
+}
+
+string result = CharsArrayToString(charsArray);
+
+Console.WriteLine(result);

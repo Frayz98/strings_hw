e8149e4d643c6553e0101852b09c681345c06ccc
Vladislav Vaga <frayz0498@gmail.com>
2024-04-06 09:31:26 +0500

    Решил третью задачу

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите произвольную строку");
+string userInput = Console.ReadLine();
+
+bool CheckIsPalindrome(string userInput) {
+    for (int i = 0; i < userInput.Length / 2; i++) {
+        if (userInput[i] != userInput[userInput.Length - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool result = CheckIsPalindrome(userInput);
+Console.WriteLine(result ? "Да" : "Нет");

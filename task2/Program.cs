3062c8abe793247730ad171d97ea126a6ef54d93
Vladislav Vaga <frayz0498@gmail.com>
2024-04-06 09:15:22 +0500

    Решил вторую задачу

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string myString = "AbCdEf-!";
+
+static string UpperToLower(string myString)
+{
+    string result = "";
+
+    for (int i = 0; i < myString.Length; i++)
+    {
+        char temp = myString[i];
+        if (Char.IsUpper(temp))
+        {
+            result += Char.ToLower(temp);
+        }
+        else
+        {
+            result += temp;
+        }
+    }
+
+    return result;
+}
+
+string result = UpperToLower(myString);
+Console.WriteLine(result);
